30a12f844240bb1c32a2a02a18807c3d1de56a9b
Malika_seleyeva <malika.seleyeva@atlascopco.com>
2024-12-09 09:03:01 +0100

    API change

--- AlphavantagaAPI.py
+++ AlphavantagaAPI.py
@@ -2,15 +2,14 @@ import requests
 import pandas as pd
 import spark 
 # make a class for API call 
-class AlphavantageAPI: 
-    def __init__(self,stock):
+
+class AlphavantageAPI:
+    def __init__(self, stock):
         self.url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stock}&apikey=53HU7S3992V5RPQZ'
         r = requests.get(self.url, verify=False)
         data = r.json()
-        data = pd.DataFrame(data)
+        data = pd.DataFrame(data['Time Series (Daily)']).T  # Transpose to get dates as rows
         columns = data.columns.tolist()
-        return print(data,columns)
+        print(data, columns)
 
-print(AlphavantageAPI("IBM"))
 data = AlphavantageAPI("IBM")
-# print(data['symbol'])S
