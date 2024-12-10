1c05be658c2dabb2edad6d9a4f1dc1f9771686f3
Malika_seleyeva <malika.seleyeva@atlascopco.com>
2024-12-10 15:53:14 +0100

    change

--- BankInvestment.py
+++ BankInvestment.py
@@ -1,4 +1,4 @@
-from trans_func import Transactions
+from trans_func import Transactions, Stocks
 from Users import User
 
 if __name__ == "__main__":
@@ -29,11 +29,15 @@ if __name__ == "__main__":
         while True:
             action = input("Do you want to buy or sell stocks? (buy/sell/exit): ")
             if action == 'buy':
-                stock = input("Enter the stock you want to buy: ")
+                stock_keyword = input("Enter a keyword to find the symbol of the stock you want:")
+                print(Stocks(stock_keyword))
+                stock = input("Enter the symbol of the stock you want to buy: ")
                 amount = int(input("Enter the number of shares you want to buy: "))
                 transactions.buy(AccessType[0],stock,amount)
             elif action == 'sell':
-                stock = input("Enter the stock you want to sell: ")
+                stock_keyword = input("Enter a keyword to find the symbol of the stock you want:")
+                print(Stocks(stock_keyword))
+                stock = input("Enter the symbol of the stock you want to sell: ")
                 amount = int(input(f"Enter the number of shares of {stock} you want to sell: "))
                 transactions.sell(AccessType[0],stock,amount)
             elif action == 'exit':
@@ -46,6 +50,8 @@ if __name__ == "__main__":
         while True:
             action = input("Do you want to buy? (buy/exit): ")
             if action == 'buy':
+                stock_keyword = input("Enter a keyword to find the symbol of the stock you want:")
+                print(Stocks(stock_keyword))
                 stock = input("Enter the stock you want to buy: ")
                 amount = int(input("Enter the number of shares you want to buy: "))
                 transactions.buy(AccessType[0],stock,amount)
@@ -63,6 +69,8 @@ if __name__ == "__main__":
             if action == 'buy':
                 print("You don't have the right access to buy stocks.")
             elif action == 'sell':
+                stock_keyword = input("Enter a keyword to find the symbol of the stock you want:")
+                print(Stocks(stock_keyword))
                 stock = input("Enter the stock you want to sell: ")
                 amount = int(input(f"Enter the number of shares of {stock} you want to sell: "))
                 transactions.buy(AccessType[0],stock,amount)
