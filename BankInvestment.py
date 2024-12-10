7286815a507631e9872e2b592258840fb7d7fed7
Malika_seleyeva <malika.seleyeva@atlascopco.com>
2024-12-10 14:53:55 +0100

    mistake fix

--- BankInvestment.py
+++ BankInvestment.py
@@ -24,31 +24,31 @@ if __name__ == "__main__":
     else: 
         print("The input you gave was wrong.")
 
-    if Access[1] == "Full":
+    if AccessType[1] == "Full":
         transactions = Transactions()
         while True:
             action = input("Do you want to buy or sell stocks? (buy/sell/exit): ")
             if action == 'buy':
                 stock = input("Enter the stock you want to buy: ")
                 amount = int(input("Enter the number of shares you want to buy: "))
-                transactions.buy(Access[0],stock,amount)
+                transactions.buy(AccessType[0],stock,amount)
             elif action == 'sell':
                 stock = input("Enter the stock you want to sell: ")
                 amount = int(input(f"Enter the number of shares of {stock} you want to sell: "))
-                transactions.sell(Access[0],stock,amount)
+                transactions.sell(AccessType[0],stock,amount)
             elif action == 'exit':
                 break
             else:
                 print("Invalid option. Please enter 'buy', 'sell', or 'exit'.")
     
-    if Access[1] == "Buy":
+    if AccessType[1] == "Buy":
         transactions = Transactions()
         while True:
             action = input("Do you want to buy? (buy/exit): ")
             if action == 'buy':
                 stock = input("Enter the stock you want to buy: ")
                 amount = int(input("Enter the number of shares you want to buy: "))
-                transactions.buy(Access[0],stock,amount)
+                transactions.buy(AccessType[0],stock,amount)
             elif action == 'sell':
                 print("You don't have the right access to sell stocks.")
             elif action == 'exit':
@@ -56,7 +56,7 @@ if __name__ == "__main__":
             else:
                 print("Invalid option. Please enter 'buy', or 'exit'.")
 
-    if Access[1] == "Sell":
+    if AccessType[1] == "Sell":
         transactions = Transactions()
         while True:
             action = input("Do you want to sell? (sell/exit): ")
@@ -65,7 +65,7 @@ if __name__ == "__main__":
             elif action == 'sell':
                 stock = input("Enter the stock you want to sell: ")
                 amount = int(input(f"Enter the number of shares of {stock} you want to sell: "))
-                transactions.buy(Access[0],stock,amount)
+                transactions.buy(AccessType[0],stock,amount)
             elif action == 'exit':
                 break
             else:
